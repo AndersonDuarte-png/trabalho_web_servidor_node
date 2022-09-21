a7e33e24f774e7029644870b69fe696b62927caf
AndersonDuarte-png <anderson.duarte889@gmail.com>
2022-09-21 20:24:42 -0300

    iniciando inputs

---
+++ servidor/front/index.js
@@ -0,0 +1,13 @@
+const input_titulo_tarefa = document.getElementById("input_titulo_tarefa")
+const input_tarefa_descricao = document.getElementById("input_tarefa_descricao")
+const button_tarefa = document.getElementById("button_tarefa")
+
+button_tarefa.addEventListener('click', async (e) => {
+
+    const input_title = input_titulo_tarefa.value
+    const input_descricao = input_tarefa_descricao.value
+    
+
+}
+
+)
