3202107e225ee5e3c1dc90e47f48c91d0d6c62a6
AndersonDuarte-png <anderson.duarte889@gmail.com>
2022-10-02 00:16:26 -0300

    criando metodos padrões do server

--- servidor/back/index.js
+++ servidor/back/index.js
@@ -1,34 +1,50 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-const bp = require('body-parser')
-
+const bp = require('body-parser');
 app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
 
 var ID = 0;
 var lista_tarefas = []
 
-const corpo_lista_tarefa = {
-    id: gerar_new_id(),
-    titulo:"titulo novo",
-    descricao:"descricao",
-    complete: false
+const exemplo_lista_tarefa = {
+    "id": "",
+    //"titulo":"exemplo de tarefa",
+    "descricao":"exemplo de tarefa",
+    "complete": false
 
 }
 
-//criar_tarefa(tarefa)
+criar_tarefa(exemplo_lista_tarefa)
 
 function gerar_new_id (){
     return ID += 1
 }
 
 function criar_tarefa(tarefa){
-    let nova_tarefa = {...tarefa, id: gerar_new_id(),complete: false}
+    //console.log(tarefa)
+    let nova_tarefa = {"id": gerar_new_id(),"descricao": tarefa["descricao"],"complete": false}
     lista_tarefas.push(nova_tarefa)
 }
 
+function alterar_tarefa(tarefa,id){
+    lista_tarefas.forEach(alterar_lista)
+    function alterar_lista(objt){
+        if(Number(objt.id) === id){
+            objt.descricao = tarefa.descricao
+            objt.complete = tarefa.complete
+        }
+    }
+}
 
+function deletar_tarefa(id){
+    //tarefa passada como argumento para possivis validações futuras
+    let index = lista_tarefas.map(objt => objt.id).indexOf(id)
+
+    if (index > -1){
+        lista_tarefas.splice(index,1)
+    }
+}
 
 function trazerListaTarefa (){
     return lista_tarefas
@@ -39,11 +55,22 @@ app.get("/lista", (req, res) => {
     res.status(200).json({status: 'ok', lista});
 });
 
-app.post("/inserir_lista", (req, res) => {
-    console.log(req.body)
+app.post("/inserir_tarefa", (req, res) => {
+    criar_tarefa(req.body)
     let lista = trazerListaTarefa()
     res.status(200).json({status: 'ok', lista});
 });
 
+app.put("/alterar_tarefa/:id",(req,res) =>{
+    alterar_tarefa(req.body, Number(req.params.id))
+    let lista = trazerListaTarefa()
+    res.status(200).json({status: 'ok', lista})
+});
+
+app.delete("/excluir_tarefa/:id",(req,res) =>{
+    deletar_tarefa(Number(req.params.id))
+    let lista = trazerListaTarefa()
+    res.status(200).json({status: 'ok', lista})
+})
 
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
